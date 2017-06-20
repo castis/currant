73b103d0fdc6afe3e4ecf115dd90c8d6d4a934b6
Daniel Sibitzky <dan@mobelux.com>
2017-06-20 19:49:45 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+from time import sleep
+from pprint import pprint
+
+import logging
+import RPi.GPIO as GPIO
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger()
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+class Motor():
+    max_duty_cycle = 80
+    min_duty_cycle = 30
+    duty_cycle_range = None
+
+    pin = None
+
+    def __init__(self, pin=None):
+        # values below 20 produce what appears to be an alarming beep
+        # and values above like 90 dont produce much of a change
+        # so keep an range and adjust the throttle within it
+        self.duty_cycle_range = self.max_duty_cycle - self.min_duty_cycle
+
+        GPIO.setup(pin, GPIO.OUT)
+
+        # 500 appears to reduce the amount of popping from the motor
+        # as long as a prop is installed
+        self.pin = GPIO.PWM(pin, 500)
+        self.pin.start(0) # duty cycle
+
+    def set_throttle(self, percentage):
+        try:
+            percentage = int(percentage)
+            if max(min(100, percentage), 0) != percentage:
+                raise ValueError
+        except ValueError:
+            # fail shut
+            percentage = 0
+
+        duty_cycle = self.min_duty_cycle + ((percentage * self.duty_cycle_range) / 100)
+        self.pin.ChangeDutyCycle(duty_cycle)
+
+
+class Vehicle():
+    #cw ccw
+    # 0  2
+    #  \/
+    #  /\
+    # 3  1
+    #ccw cw
+    motors = []
+
+    def __init__(self, motors):
+        self.motors = motors
+
+    # def balance(self):
+    #     pass
+
+    # def set_throttle(self, throttle, motor=None):
+    #     if motor:
+    #         self.motors[motor].set_throttle(threottle)
+    #     else:
+    #         for motor in motors:
+    #             motor.set_throttle(throttle)
+
+    def shutdown(self):
+        # self.set_throttle(0)
+        GPIO.cleanup()
+        print(" shutdown")
+
+
+vehicle = Vehicle([
+    # Motor(pin=17),
+    # Motor(pin=18),
+    # Motor(pin=19),
+    # Motor(pin=20),
+])
+
+motor = Motor(pin=17)
+
+try:
+    while True:
+        throttle = raw_input("set throttle: ")
+        motor.set_throttle(throttle)
+
+except KeyboardInterrupt:
+    vehicle.shutdown()
---
+++ sync.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# kill $(ps aux | grep sync.p[y] | awk '{print $2}')
+
+import sys
+import os
+import paramiko
+import time
+import logging
+import psutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger()
+
+
+class SFTPClient(paramiko.SFTPClient):
+    """ subclass that recursively sync a directory tree """
+    def put_dir(self, source, target):
+        """ target folder needs to preexist """
+        for item in os.listdir(source):
+            source_path = os.path.join(source, item)
+            target_path = '%s/%s' % (target, item)
+
+            if os.path.isfile(source_path):
+                self.put(source_path, target_path)
+            else:
+                self.mkdir(target_path, ignore_existing=True)
+                self.put_dir(source_path, target_path)
+
+    def mkdir(self, path, mode=511, ignore_existing=False):
+        try:
+            super(SFTPClient, self).mkdir(path, mode)
+        except IOError:
+            if not ignore_existing:
+                raise
+
+
+class FSEventHandler(FileSystemEventHandler):
+    source = '.'
+    target = '/root/flightcontroller'
+
+    def on_modified(self, event):
+        # if this file was modified
+        if os.path.basename(event.src_path) == os.path.basename(__file__):
+            logger.info("restarting sync daemon")
+            try:
+                p = psutil.Process(os.getpid())
+                for handler in p.open_files() + p.connections():
+                    os.close(handler.fd)
+            except Exception as e:
+                logging.error(e)
+
+            os.execl(sys.executable, sys.executable, *sys.argv)
+            return
+
+        logger.info('syncing...')
+        try:
+            sftp.put_dir(self.source, self.target)
+        except Exception as e:
+            logger.error(e)
+
+
+hostname = '192.168.1.37'
+username = 'root'
+key_file = '/Users/dsibitzky/.ssh/id_rsa'
+
+try:
+    transport = paramiko.Transport((hostname, 22))
+    pkey = paramiko.RSAKey.from_private_key_file(key_file)
+    transport.connect(username=username, pkey=pkey)
+    sftp = SFTPClient.from_transport(transport)
+except Exception as e:
+    logger.error(e)
+
+observer = Observer()
+observer.schedule(FSEventHandler(), '.', recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+    sftp.close()
+    transport.close()
+    sys.exit(0)
+
+observer.join()
